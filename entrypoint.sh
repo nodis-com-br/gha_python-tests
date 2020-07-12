0b6ba3f9610d8e3285e4b2e319dcdf5e58b6d069
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-12 15:31:24 -0300

    Fixed entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export PIP_INDEX_URL="http://${NODIS_PYPI_USER}:${NODIS_PIPY_PASSWORD}@${NODIS_PYPI_HOST}/simple"
+export PIP_INDEX_URL="https://${NODIS_PYPI_USER}:${NODIS_PIPY_PASSWORD}@${NODIS_PYPI_HOST}/simple"
 export PIP_EXTRA_INDEX_URL="https://pypi.org/simple/"
 
 if [[ ${NODIS_NO_TESTS} != "True" ]] && [[ -f requirements.txt ]]; then
 
-    pip install -r requirements.txt --user
+    pip install -H -r requirements.txt --user
 
     if python -m coverage run --source='.' -m pytest; then
         echo "Ok"
