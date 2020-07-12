ef95c0c26486d877449c67f65a7bdbc89261d35b
Pedro Tonini <pedro.tonini@nodis.com.br>
2020-07-12 15:46:06 -0300

    Edited entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export PIP_INDEX_URL="https://${NODIS_PYPI_USER}:${NODIS_PIPY_PASSWORD}@${NODIS_PYPI_HOST}/simple"
+export PIP_INDEX_URL="https://${NODIS_PYPI_USER}:${NODIS_PYPI_PASSWORD}@${NODIS_PYPI_HOST}/simple"
 export PIP_EXTRA_INDEX_URL="https://pypi.org/simple/"
 
 if [[ ${NODIS_NO_TESTS} != "True" ]] && [[ -f requirements.txt ]]; then
